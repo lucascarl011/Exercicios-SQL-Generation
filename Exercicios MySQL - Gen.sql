c2c25df3614d0849fae3a1a6e71ce2c1aec76d1b
lucascarl011 <lucassscarlosss54@gmail.com>
2024-06-27 15:07:16 -0300

    Primeiro commit SQL

--- Exercicios MySQL - Gen.sql
+++ Exercicios MySQL - Gen.sql
@@ -1,163 +1,35 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: db_empresa
--- ------------------------------------------------------
--- Server version	8.0.37
+CREATE DATABASE db_ecommerce;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE db_ecommerce;
 
---
--- Table structure for table `tb_funcionarios`
---
+CREATE TABLE tb_produtos (
+id BIGINT auto_increment primary key,
+produto varchar(100),
+descricao varchar(300),
+preco decimal(10,2),
+categoria varchar(100)
+);
 
-DROP TABLE IF EXISTS `tb_funcionarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tb_funcionarios` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `cargo` varchar(50) DEFAULT NULL,
-  `salario` decimal(10,2) DEFAULT NULL,
-  `data_admissao` date DEFAULT NULL,
-  `setor` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_funcionarios`
---
-
-LOCK TABLES `tb_funcionarios` WRITE;
-/*!40000 ALTER TABLE `tb_funcionarios` DISABLE KEYS */;
-INSERT INTO `tb_funcionarios` VALUES (1,'Lucas Carlos','Assistente',2900.00,'2024-02-10','TI'),(2,'Marta Carlos','Gerente',4600.00,'2021-05-05','TI'),(3,'Carlos Roberto','CEO',25000.00,'2015-09-02','TI'),(4,'Diogo Higor','Auxiliar',1800.00,'2023-07-20','TI'),(5,'Larissa Santos','Atendente',2100.00,'2018-12-05','RH'),(6,'Maria das Graças','Coordenadora',5200.00,'2019-04-03','RH');
-/*!40000 ALTER TABLE `tb_funcionarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-06-27 14:45:58
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: db_ecommerce
--- ------------------------------------------------------
--- Server version	8.0.37
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `tb_produtos`
---
-
-DROP TABLE IF EXISTS `tb_produtos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tb_produtos` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `produto` varchar(100) DEFAULT NULL,
-  `descricao` varchar(300) DEFAULT NULL,
-  `preco` decimal(10,2) DEFAULT NULL,
-  `categoria` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_produtos`
---
-
-LOCK TABLES `tb_produtos` WRITE;
-/*!40000 ALTER TABLE `tb_produtos` DISABLE KEYS */;
-INSERT INTO `tb_produtos` VALUES (1,'Nike - Air Force 1','A junção do esporte e o casual',780.00,'Sneakers'),(2,'Adidas - SuperStar Retrô','O sucesso dos anos 1980 está de volta',650.00,'Sneakers'),(3,'Lacoste - Camisa Polo','A elegânia e conforto ao seu alcance',340.00,'Casual'),(4,'Lacoste - Tênis Europa','A aposta certa para todos os momentos',299.00,'Casual'),(5,'Nike - Agasalho Sportswear','Clássico que não sai de moda',280.00,'Esportivo'),(6,'Mizuno - Prophecy 13','O conforto jamais visto antes',1708.00,'Esportivo'),(7,'Tesla - Coil Malbeck','O conforto ao andar de SkateBoard',420.00,'SkateBoard'),(8,'Alexander Mcqueen - Oversized','Estilo para todas ocasiões',320.00,'Casual');
-/*!40000 ALTER TABLE `tb_produtos` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-06-27 14:45:58
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: db_escola
--- ------------------------------------------------------
--- Server version	8.0.37
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `tb_alunos`
---
-
-DROP TABLE IF EXISTS `tb_alunos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tb_alunos` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) DEFAULT NULL,
-  `nota` decimal(10,2) DEFAULT NULL,
-  `ano` int DEFAULT NULL,
-  `professor` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_alunos`
---
-
-LOCK TABLES `tb_alunos` WRITE;
-/*!40000 ALTER TABLE `tb_alunos` DISABLE KEYS */;
-INSERT INTO `tb_alunos` VALUES (1,'Lucas',7.00,3,'Julio'),(2,'Marta',8.00,2,'Julio'),(3,'Hernane',4.00,5,'José'),(4,'Tiffany',5.00,3,'Vanuza'),(5,'Samuel',10.00,4,'Julio'),(6,'Neymar',10.00,5,'Dorival JR'),(7,'Endrick',8.00,1,'Abel'),(8,'Yuri Alberto',10.00,3,'Antônio');
-/*!40000 ALTER TABLE `tb_alunos` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-06-27 14:45:58
+INSERT INTO tb_produtos (produto, descricao, preco, categoria)
+VALUES
+	("Nike - Air Force 1", "A junção do esporte e o casual", 780.00, "Sneakers"),
+    ("Adidas - SuperStar Retrô", "O sucesso dos anos 1980 está de volta", 650.00, "Sneakers"),
+    ("Lacoste - Camisa Polo", "A elegânia e conforto ao seu alcance", 340.00, "Casual"),
+    ("Lacoste - Tênis Europa", "A aposta certa para todos os momentos", 299.00, "Casual"),
+    ("Nike - Agasalho Sportswear", "Clássico que não sai de moda", 280.00, "Esportivo"),
+    ("Mizuno - Prophecy 13", "O conforto jamais visto antes", 1708.00, "Esportivo"),
+    ("Tesla - Coil Malbeck", "O conforto ao andar de SkateBoard", 420.00, "SkateBoard"),
+    ("Alexander Mcqueen - Oversized", "Estilo para todas ocasiões", 480.00, "Casual");
+    
+    SELECT * FROM tb_produtos where preco > 500;
+    
+    SELECT * FROM tb_produtos where preco < 500;
+    
+    UPDATE tb_produtos SET preco = 320.00 WHERE produto = "Alexander Mcqueen - Oversized";
+    
+    SELECT * FROM tb_produtos;
+    
+    
+  
+    
+    
